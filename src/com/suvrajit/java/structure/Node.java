e4764225807ea25c43d7c1acde0f1714d6805991
Suvrajit Manna <suvrajit.manna@sap.com>
2017-11-10 09:44:20 +0530

    initial commmits

---
+++ src/com/suvrajit/java/structure/Node.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.suvrajit.java.structure;
+
+/**
+ *
+ * @author I327917
+ */
+public class Node {
+    int key;
+    Node left, right;
+    
+    public Node(int item){
+        this.key = item;
+        this.left = null;
+        this.right = null;
+    }
+    
+    Node (){
+        
+    }
+}
